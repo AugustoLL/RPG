84aa6b0ec5b272d03338508f128152a5d23e7ae5
AugustoLL <37594817+AugustoLL@users.noreply.github.com>
2020-07-26 17:08:59 +0000

    Add files via upload

--- RPG/Overlap/Hitbox.gd
+++ RPG/Overlap/Hitbox.gd
@@ -1,3 +1,13 @@
 extends Area2D
 
 export var damage = 1
+var player = null
+
+func canHitPlayer():
+	return player != null
+
+func _on_Hitbox_body_entered(body):
+	player = body
+
+func _on_Hitbox_body_exited(_body):
+	player = null
